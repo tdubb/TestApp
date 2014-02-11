b040569c3a2cf496855d90d21ce80f624e12cdfd
Tyler Wasden <twasden@gmail.com>
2014-02-11 15:00:45 -0700

    first commit

---
+++ app/database/migrations/2014_02_08_190620_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->string('text');
+			$table->string('author');
+
+			$table->timestamps();
+		});
+	}
+		
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('comments', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
---
+++ app/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('index');
+});
+
+Route::group(array('prefix' => 'api'), function() {
+
+  // since we will be using this just for CRUD, we won't need create and edit
+  // Angular will handle both of those forms
+  // this ensures that a user can't access api/create or api/edit when there's nothing there
+  Route::resource('comments', 'CommentController', 
+    array('only' => array('index', 'store', 'destroy')));
+});
+
+// =============================================
+// CATCH ALL ROUTE =============================
+// =============================================
+// all routes that are not home or api will be redirected to the frontend
+// this allows angular to route them
+App::missing(function($exception)
+{
+  return View::make('index');
+});

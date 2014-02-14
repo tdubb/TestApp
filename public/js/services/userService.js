e6a637e068bce139bf6a8e4a492551a41c17e3b8
Tyler Wasden <twasden@gmail.com>
2014-02-14 10:35:54 -0700

    cleaned up comments

--- public/js/services/userService.js
+++ public/js/services/userService.js
@@ -3,16 +3,10 @@ angular.module('userService', [])
   .factory('User', function($http) {
 
     return {
-      // get all the comments
       get : function() {
         return $http.get('/test/TestApp/public/api/users');
       },
 
-      // get : function() {
-      //   return $http.get('/test/TestApp/public/api/users' + id);
-      // },
-
-      // save a comment (pass in comment data)
       save : function(userData) {
         return $http({
           method: 'POST',
@@ -22,7 +16,6 @@ angular.module('userService', [])
         });
       },
 
-      // destroy a comment
       destroy : function(id) {
         return $http.delete('/test/TestApp/public/api/user' + id);
       }
